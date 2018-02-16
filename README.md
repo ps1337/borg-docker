5f308bc62d8d3350d496bd26613c8a4328c15a07
ps <ps@ps.de>
2018-02-16 08:13:24 +0100

    use alpine

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # A dockerized Borg server
 
-This is basically just a merged version of these images:
+This repo contains two variants of a dockerized borg server
+
+- Using a minimal alpine base image (size < 100 MB)
+- Using a debian base image
+
+`Dockerfile.debian` is basically just a merged version of these images:
 
 `sebcworks/borgbackup-server` and `sebcworks/borgbackup-base`
+
+which use debian as base image.
 
 The only change is that `borg` gets installed via the latest release from GitHub instead of using pip because otherwise with the missing native msgpack, performance is < 0.
 
 Use `make build` and `make run` to start a dockerized borg instance. Please check the `Makefile` for things you have to configure.
-You can also use the built image from docker hub using `docker pull bananafett/borg-docker` :)
+You can also use the built image from docker hub using `docker pull bananafett/borg-docker` which uses the alpine base image.
 
-All credits go to sebcworks :)
+All credits for the debian Dockerfile go to sebcworks :)
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,24 +3,25 @@
 if [ "x$1" = "xstart" ]; then
     # Check if we have the authorized_keys file
     if [ ! -f "/home/borg/.ssh/authorized_keys" ]; then
-	echo "Missing authorized_keys file. Won't continue."
-	exit 1
+    echo "Missing authorized_keys file. Won't continue."
+    exit 1
     fi
+    chmod 600 /home/borg/.ssh/authorized_keys
 
     # Ensure that key files of the server exists
     if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
-	echo "INFO: No SSH host key present, creating them..."
-	/usr/bin/ssh-keygen -A
+    echo "INFO: No SSH host key present, creating them..."
+    /usr/bin/ssh-keygen -A
     fi
 
     # Ensure that /var/backups/borg is for "us"
     if [ ! -d "/var/backups/borg" ]; then
-	echo "Missing repository folder at /var/backups/borg. Won't continue."
-	exit 1
+    echo "Missing repository folder at /var/backups/borg. Won't continue."
+    exit 1
     fi
     if [ "`ls -ld /var/backups/borg | awk '{print $3}'`" != "borg" ]; then
-	chown borg.borg /var/backups/borg
-	chmod 750 /var/backups/borg
+    chown borg.borg /var/backups/borg
+    chmod 750 /var/backups/borg
     fi
 
     # Launch ssh server as daemon
